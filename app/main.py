4c1d48b8efa39affbc042c2a03d42c34e65667f8
Dmitrii Ivanov <dmitriy.site1@gmail.com>
2024-07-19 22:12:15 -0300

    Implement API KEY authentication

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Security
 
+from app.auth.security import get_api_key
 from app.image.handler import handler_image
 from app.image.schemas import SImage, SWatermark
 
@@ -13,6 +14,7 @@ app = FastAPI(
 @app.post("/process-image")
 async def process_image_endpoint(
         image: SImage = Depends(),
-        watermark: SWatermark = Depends()
+        watermark: SWatermark = Depends(),
+        api_key: str = Security(get_api_key)
 ):
     return await handler_image(image, watermark)
